d3de0879243444b628df99a3e6671dabd2a2a37a
22 <60903333+nini22P@users.noreply.github.com>
2023-01-24 00:47:46 +0800

    Multi-language support

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,9 +1,15 @@
+const default_lang_url = "lang/en-US.json";
+const lang_url = "lang/" + navigator.language.toString() + ".json";
 const data_url = "https://skyline-builds.alula.gay/builds";
 const download_url = "https://skyline-builds.alula.gay/cache/"
 const commit_url = "https://github.com/skyline-emu/skyline/commit/"
 
-loadUrl(data_url).then((data) => {
-    insert(data);
+loadLang(lang_url).then((lang_data) => {
+    setLang(lang_data);
+    loadUrl(data_url).then((data) => {
+        insert(lang_data, data);
+        document.getElementById("loading").style.display = "none";
+    });
 });
 
 async function loadUrl(data_url) {
@@ -11,7 +17,27 @@ async function loadUrl(data_url) {
     return data = await response.json();
 }
 
-function insert(data) {
+async function loadLang(lang_url) {
+    try {
+        return getLang(lang_url);
+    } catch (error) {
+        return getLang(default_lang_url);
+    }
+}
+
+function getLang(lang_url) {
+    const request = new XMLHttpRequest();
+    request.open("GET", lang_url, false);
+    request.send("");
+    return JSON.parse(request.response);
+}
+
+function setLang(lang_data) {
+    document.getElementById("loading").textContent = lang_data.loading;
+    document.getElementById("note").innerHTML = lang_data.note;
+}
+
+function insert(lang_data, data) {
     const skylines = document.getElementById("skylines");
     const t = document.querySelector("#temp");
     const createdAt = t.content.querySelector(".createdAt")
@@ -23,7 +49,8 @@ function insert(data) {
     for (let i = 0; i < data.length; i++) {
         createdAt.textContent = new Date(data[i].createdAt).toLocaleString();
         number.textContent = "#" + data[i].runNumber;
-        branch.textContent = "分支: " + data[i].branch;
+        branch.textContent = lang_data.branch + ": " + data[i].branch;
+        download.textContent = lang_data.download;
         download.href = download_url + data[i].id + "/" + data[i].apkName;
         commit.href = commit_url + "/" + data[i].commit.id;
         message.textContent = data[i].commit.message.substring(0, data[i].commit.message.indexOf("\n\n") + 1) ? data[i].commit.message.substring(0, data[i].commit.message.indexOf("\n\n") + 1) : data[i].commit.message;
--- app/src/main/java/io/github/nini22p/skylinechecker/MainActivity.kt
+++ app/src/main/java/io/github/nini22p/skylinechecker/MainActivity.kt
@@ -1,26 +1,19 @@
 package io.github.nini22p.skylinechecker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.github.nini22p.skylinechecker.ui.theme.SkylineCheckerTheme
 
+@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val myWebView = WebView(this)
         setContentView(myWebView)
         myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.allowFileAccessFromFileURLs = true;
+        myWebView.settings.allowUniversalAccessFromFileURLs = true;
         myWebView.loadUrl("file:///android_asset/index.html")
     }
 }
